7e211b58cea2c074d86cd469e9c012539e104474
Gilles Vanderseypen <gillesvanderseypen@gmail.com>
2021-09-06 14:41:01 +0200

    test

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ Object.prototype.ancestors = function () {
   return array.reverse();
 }
 
-console.log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
-console.log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
-console.log(bar.ancestors());  // returns ['foo', 'Object.prototype']
-console.log(foo.ancestors());  // returns ['Object.prototype']
+// console.log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+// console.log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+// console.log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+// console.log(foo.ancestors());  // returns ['Object.prototype']
